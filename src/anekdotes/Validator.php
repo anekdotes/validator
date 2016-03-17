bf65c84e7ab0dad71a1d852f151db19cc126d046
Sébastien Roy <sebastien.roy@anekdotes.com>
2016-03-17 13:57:33 -0400

    license and readme

--- src/anekdotes/Validator.php
+++ src/anekdotes/Validator.php
@@ -1,4 +1,16 @@
-<?php namespace Anekdotes;
+<?php
+
+/*
+ * This file is part of the Validator package.
+ *
+ * (c) Anekdotes Communication inc. <info@anekdotes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Anekdotes;
+
 use Carbon\Carbon;
 
 /**
